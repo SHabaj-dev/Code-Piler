8498ebd1510eeb4c45799d58f5b2cf61a9493d21
SHabaj-dev <shabajansari84@gmail.com>
2023-02-16 22:55:41 +0530

    Added Animation

--- app/src/main/java/com/sbz/code_piler/MainActivity.kt
+++ app/src/main/java/com/sbz/code_piler/MainActivity.kt
@@ -2,10 +2,27 @@ package com.sbz.code_piler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.sbz.code_piler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnRun.setOnClickListener {
+            showOutput()
+        }
+
+
+    }
+
+    fun showOutput(){
+        binding.tvOutput.visibility = View.VISIBLE
     }
 }
--- app/src/main/java/com/sbz/code_piler/SplashScreen.kt
+++ app/src/main/java/com/sbz/code_piler/SplashScreen.kt
@@ -4,29 +4,40 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.AnimationUtils
+import androidx.databinding.DataBindingUtil
 import com.sbz.code_piler.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
+    @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-//        setAnimation()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen)
+        setAnimation()
+        hideNavAndStatusBar()
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 2000)
 //        setAnimation()
+    }
+
+    @Suppress("DEPRECATION")
+    fun hideNavAndStatusBar(){
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
     }
 
     private fun setAnimation(){
 //        val mAnimation = AnimationUtils.loadAnimation(this@SplashScreen, R.anim.image_animation)
-//        val fadeIn = AlphaAnimation(0f, 1f)
-//        fadeIn.duration = 1000
-//        binding.ivSplashScreen.startAnimation(mAnimation)
+        val fadeIn = AlphaAnimation(1f, 0f)
+        fadeIn.duration = 2600
+        binding.ivSplashScreen.startAnimation(fadeIn)
+        binding.tvSplashScreen.startAnimation(fadeIn)
     }
 }
