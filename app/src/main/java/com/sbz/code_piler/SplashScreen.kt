3a30f4121afa9d890e1d2a84cd6cf3a252155e3a
SHabaj-dev <shabajansari84@gmail.com>
2023-02-16 23:56:39 +0530

    Fixed Some Spinner

--- app/src/main/java/com/sbz/code_piler/SplashScreen.kt
+++ app/src/main/java/com/sbz/code_piler/SplashScreen.kt
@@ -6,27 +6,28 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AlphaAnimation
 import android.view.animation.AnimationUtils
+import androidx.databinding.DataBindingUtil
 import com.sbz.code_piler.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
+    @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-//        setAnimation()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen)
+        setAnimation()
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 2500)
 //        setAnimation()
     }
 
     private fun setAnimation(){
-//        val mAnimation = AnimationUtils.loadAnimation(this@SplashScreen, R.anim.image_animation)
-//        val fadeIn = AlphaAnimation(0f, 1f)
-//        fadeIn.duration = 1000
-//        binding.ivSplashScreen.startAnimation(mAnimation)
+        val fadeIn = AlphaAnimation(0f, 1f)
+        fadeIn.duration = 2000
+        binding.ivSplashScreen.startAnimation(fadeIn)
     }
 }
