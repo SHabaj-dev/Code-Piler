1fbc84d9b2d1044359c0eeac73506eb02466c999
pr847 <Sonukbc6@gmail.com>
2023-02-19 11:49:02 +0530

    added appi to send data

--- app/src/main/java/com/sbz/code_piler/MainActivity.kt
+++ app/src/main/java/com/sbz/code_piler/MainActivity.kt
@@ -1,25 +1,150 @@
 package com.sbz.code_piler
 
+
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.textfield.TextInputEditText
 import com.sbz.code_piler.databinding.ActivityMainBinding
 import com.sbz.code_piler.utils.CodeHighlighter
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import org.json.JSONObject
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mTextViewResult: TextView
+    private lateinit var editText: TextInputEditText
+    private lateinit var btn: AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
+        // mTextViewResult=findViewById(R.id.tv_output)
+
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil
             .setContentView(this, R.layout.activity_main)
+        editText = findViewById(R.id.tv_editor)
+        btn = findViewById(R.id.btn_run)
 
         setSpinnerItem()
         setImageResource()
+
+        //sending
+        /*val client = OkHttpClient()
+
+// Create a JSON object with the data to be posted
+        val json = JSONObject()
+        json.put("lang", "c++")
+        json.put("input", "")
+        json.put("code", "console.log")
+
+// Create a request body with the JSON data
+        val requestBody = RequestBody.create("application/json".toMediaTypeOrNull(), json.toString())
+
+// Create a request object with the API endpoint URL and the request body
+        val request = Request.Builder()
+            .url("https://example.com/api")
+            .post(requestBody)
+            .build()
+
+// Send the request asynchronously
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle the failure here
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                // Handle the response here
+            }
+        })*/
+
+        //Create a JSON object with the data to be posted
+        /*val json = JSONObject()
+        json.put("name", "prabhat")
+        json.put("job", "leader")*/
+        /*val editTextValue = editText.text.toString()*/
+
+
+       /* val json = JSONObject()
+        json.put("lang", "java")
+        json.put("input", "")
+        json.put("code", editTextValue)*/
+
+
+        /*val dataModel=DataModel("java","",editTextValue)
+        val json2=JSONObject(dataModel)
+        val jsonString=json2.toString()*/
+        /*
+        String program = "public class HelloWorld {\n" +
+                "    public static void main(String[] args) {\n" +
+                "        System.out.println(\"Hello, World!\");\n" +
+                "    }\n" +
+                "}";
+
+        */
+        val program = "#include <iostream>\r\n\r\nint main() {\r\n    std::cout << \"Hello World!\";\r\n  return 0;\r\n}"
+        val editTextValue = editText.setText(program).toString()
+         val json = JSONObject()
+        json.put("lang", "cpp")
+        json.put("input", "")
+        json.put("code", program)
+
+
+
+
+        val client = OkHttpClient()
+
+        //https://reqres.in/api/users
+        val url = "https://reqres.in/api/users"
+        val myurl = "http://127.0.0.1:5000/compile"
+        // Create a request body with the JSON data
+        btn.setOnClickListener {
+            val requestBody =
+                RequestBody.create("application/json".toMediaTypeOrNull(), json.toString())
+
+
+            val request: Request = Request.Builder()
+                .url(myurl)
+                .post(requestBody)
+                .build()
+
+
+            client.newCall(request).enqueue(object : Callback {
+                /*fun onFailure(call: Call?, e: IOException) {
+                    e.printStackTrace()
+                }
+
+                @Throws(IOException::class)
+                fun onResponse(call: Call?, response: Response) {
+                    if (response.isSuccessful) {
+                        val myResponse = response.body!!.string()
+                        runOnUiThread { mTextViewResult.setText(myResponse) }
+                    }
+                }*/
+
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if (response.isSuccessful) {
+                        val myResponse = response.body!!.toString()
+                        //Log.d("PrabhatTiwari","myResponse")
+                        Log.d("onResponse", response.body!!.string())
+                        //runOnUiThread(mTextViewResult.setText(myResponse))
+                    }
+                }
+            })
+
+        }
 
 
     }
@@ -72,9 +197,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-
-
 
 
 }
